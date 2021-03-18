24ab11d444920751b1822aa3f044732d614e2fca
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-03-18 13:16:46 +0100

    login working

--- render/home.js
+++ render/home.js
@@ -1,3 +1,73 @@
+import uniqid from 'uniqid';
+import fs, { writeFile } from 'fs';
+
 export function homeRoute(req, res) {
     res.render('index', { title: "Home"});
+}
+
+export function userRoute(req, res) {
+    res.render('createuser', { title: "Maak een account", userid: uniqid.time()});
+}
+
+export function dashboardRoute(req, res) {
+    const logincode = req.body.logincode;
+    console.log(logincode);
+    if(logincode && logincode.length === 8) {
+        // Get the data file
+        const dataFile = fs.readFileSync("./data/data.json");
+        // Convert it to an array
+        const data = Array.from(JSON.parse(dataFile));
+        // Filter ids on provided logincode
+        const ids = data.map(user => user.userid).filter(id => id == logincode);
+
+        if(ids.length === 1) {
+           const index = data.map(user => user.userid).indexOf(logincode);
+           const userData = data[index];
+           res.render("dashboard", { title: "Dashboard", userData, shirtid: uniqid() });
+        }
+        else {
+            res.status(401).render("404", { title: "404 Not Found" });   
+        }
+    }
+    else {
+        res.status(404).render("404", { title: "404 Not Found" });
+    }
+}
+
+export function userCreationRoute(req, res) {
+    // Check if all required fields are received
+    if(req.body.clientid && req.body.voornaam && req.body.achternaam && req.body.email) {
+        // Get the data file
+        const dataFile = fs.readFileSync("./data/data.json");
+        // Convert it to an array
+        const data = Array.from(JSON.parse(dataFile));
+        // Filter ids on provided clientid
+        const ids = data.map(user => user.userid).filter(id => id == req.body.clientid);
+        
+        // If id excists > change that object
+        if(ids.length > 0) {
+            const index = data.map(user => user.userid).indexOf(req.body.clientid);
+            data[index].voornaam = req.body.voornaam;
+            data[index].achternaam = req.body.achternaam;
+            data[index].email = req.body.email;
+        }
+
+        // If it does not exist > Create new object
+        else {
+            const dataObject = {
+                "userid": req.body.clientid,
+                "voornaam": req.body.voornaam,
+                "achternaam": req.body.achternaam,
+                "email": req.body.email,
+                "orderedShirts": [],
+                "savedShirts": []
+            };
+            data.push(dataObject);
+        }
+        // Convert data back to JSON and write the file
+        const whatToWrite = JSON.stringify(data, null, 2);
+        fs.writeFile("./data/data.json", whatToWrite, (err) => { if(err){throw err;} console.log("succes");});
+        
+        res.render('code', { title: "Dit is je inlogcode", userid: req.body.clientid, voornaam: req.body.voornaam, shirtid: uniqid()});
+    }
 }
--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,20 @@
 import express from 'express';
-import { homeRoute } from '../render/home.js';
+import bodyParser from 'body-parser';
+import { homeRoute, userRoute, dashboardRoute, userCreationRoute } from '../render/home.js';
 
 // Thanks to Guus and Robin for helping with "export"
 export const router = express.Router();
 
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+
 // Home page (overview)
 router
     .get("/", homeRoute)
+    .get("/newuser", userRoute)
     .get("*", (req, res) => {
         res.status(404).render("404", { title: "404 Not Found" });
     });
+
+router
+    .post("/dashboard", urlencodedParser, dashboardRoute)
+    .post("/createuser", urlencodedParser, userCreationRoute);
