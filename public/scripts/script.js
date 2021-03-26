3a2c2c6ea8ac6fd3ee69000e36ba5dc933e7f641
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-03-26 09:50:41 +0100

    Code copy to clipboard

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,9 @@
-console.log("Hello");
 const fitting = document.querySelectorAll("input[name='pasvorm']");
 const colors = document.querySelectorAll("input[name='kleur']");
 const shirtTextInput = document.querySelector("input[name='shirttekst']");
 const productImg = document.querySelector("div.studio img");
 const shirtText = document.querySelector("div.studio div.preview p");
+const codeInput = document.querySelector("input#code");
 
 
 fitting.forEach(element => {
@@ -13,9 +13,11 @@ fitting.forEach(element => {
         if (kleurSelect.length > 0) {
             const shirtColor = kleurSelect[0].value;
             productImg.src = `/img/shirt${gender}${shirtColor}.png`;
+            productImg.alt = `afbeelding van een ${shirtColor} shirt voor een ${gender}`;
         } 
         else {
             productImg.src = `/img/shirt${gender}zwart.png`;
+            productImg.alt = `afbeelding van een zwart shirt voor een ${gender}`;
         }
     });
 });
@@ -27,14 +29,33 @@ colors.forEach(element => {
         if (genderSelect.length > 0) {
             const gender = genderSelect[0].value;
             productImg.src = `/img/shirt${gender}${shirtColor}.png`;
+            productImg.alt = `afbeelding van een ${shirtColor} shirt voor een ${gender}`;
         } 
         else {
             productImg.src = `/img/shirtman${shirtColor}.png`;
+            productImg.alt = `afbeelding van een ${shirtColor} shirt voor een man`;
         }
     });
 });
 
-shirtTextInput.addEventListener("input", (event) => {
-    const text = event.target.value;
-    shirtText.textContent = text;
-});
+if(shirtTextInput) {
+    shirtTextInput.addEventListener("input", (event) => {
+        const text = event.target.value;
+        shirtText.textContent = text;
+    });
+}
+
+if(codeInput) {
+    codeInput.addEventListener("click", () => {
+      codeInput.select();
+      document.execCommand("copy");
+      const codeSuccess = document.querySelectorAll("div.code p");
+
+      if(codeSuccess.length === 0) {
+        const codeContainer = document.querySelector("div.code");
+        const p = document.createElement("p");
+        p.textContent = "De code is succesvol gekopieerd naar je klembord!";
+        codeContainer.appendChild(p);
+      }
+    });
+}
